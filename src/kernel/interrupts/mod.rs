c2ab0c49bf952274736994ee75c82b888f6c6a02
HXM4Tech <68482946+HXM4Tech@users.noreply.github.com>
2022-11-13 17:32:47 +0100

    Changing stack on all interrupts + new page fault handler

--- src/kernel/interrupts/mod.rs
+++ src/kernel/interrupts/mod.rs
@@ -13,10 +13,13 @@ use crate::timer;
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        unsafe {idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(DOUBLE_FAULT_IST_INDEX as u16) };
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+
+        unsafe {
+            idt.page_fault.set_handler_fn(page_fault_handler).set_stack_index(INTERRUPT_IST_INDEX as u16);
+            idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(INTERRUPT_IST_INDEX as u16);
+            idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler).set_stack_index(INTERRUPT_IST_INDEX as u16);
+            idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler).set_stack_index(INTERRUPT_IST_INDEX as u16);
+        }
 
         idt
     };
@@ -25,7 +28,7 @@ lazy_static! {
 static TSS: Once<TaskStateSegment> = Once::new();
 static GDT: Once<gdt::Gdt> = Once::new();
 
-const DOUBLE_FAULT_IST_INDEX: usize = 0;
+const INTERRUPT_IST_INDEX: usize = 0;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -56,11 +59,11 @@ pub fn init(memory_controller: &mut MemoryController) {
     use x86_64::instructions::segmentation::{Segment, CS};
     use x86_64::instructions::tables::load_tss;
 
-    let double_fault_stack = memory_controller.alloc_stack(4).expect("could not allocate double fault stack");
+    let interrupt_stack = memory_controller.alloc_stack(4).expect("could not allocate double fault stack");
 
     let tss = TSS.call_once(|| {
         let mut tss = TaskStateSegment::new();
-        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX] = VirtAddr::new(double_fault_stack.top() as u64);
+        tss.interrupt_stack_table[INTERRUPT_IST_INDEX] = VirtAddr::new(interrupt_stack.top() as u64);
         tss
     });
 
@@ -97,9 +100,8 @@ pub fn init(memory_controller: &mut MemoryController) {
 
 extern "x86-interrupt" fn page_fault_handler(stack_frame: InterruptStackFrame, error_code: PageFaultErrorCode) {
     use x86_64::registers::control::Cr2;
-    panic!("page fault (code {:?}): accesed address: {:?}:\n{:#?}", error_code, Cr2::read(), stack_frame);
+    panic!("page fault ({:?}):\naccesed address: {:?}:\n{:#?}", error_code, Cr2::read(), stack_frame);
 }
-
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, error_code: u64) -> ! {
     panic!("double fault (code {}):\n{:#?}", error_code, stack_frame);
